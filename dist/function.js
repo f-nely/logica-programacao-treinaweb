17d009711341467b3dacbb046f5a6d941e5741ce
nelynely <nelly.nf54@gmail.com>
2025-03-31 17:34:22 -0300

    Add function.js

---
+++ dist/function.js
@@ -0,0 +1,10 @@
+"use strict";
+function sum(num1, num2) {
+    return num1 + num2;
+}
+/* console.log(sum(10, 30));
+console.log(sum(2, 4));
+console.log(sum(1, 2)); */
+let resultado = sum(15, 25);
+let resultado2 = sum(resultado, 5);
+console.log(resultado2);
