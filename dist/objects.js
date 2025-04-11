beabce0a8f53327f7941e4bf6c28158890e0697a
nelynely <nelly.nf54@gmail.com>
2025-04-11 16:08:52 -0300

    Add objects.js

---
+++ dist/objects.js
@@ -0,0 +1,12 @@
+"use strict";
+let student = {
+    name: "Viktor Gyökeres",
+    age: 26,
+    isActiv: true,
+    grade: [10, 8, 7.5, 9.5, 7],
+};
+console.log(student);
+console.log(student.name);
+console.log(student.isActiv);
+console.log(student.grade);
+console.log(student.grade[0]);
