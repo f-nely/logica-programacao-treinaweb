9c47b31665268d4ab23911e739e79d647cc92780
nelynely <nelly.nf54@gmail.com>
2025-04-19 08:56:32 -0300

    Create contador-vogal.ts

---
+++ src/contador-vogal.ts
@@ -0,0 +1,15 @@
+let palavraMisteriosa: string = "treinaweb";
+
+let qtdVogal = 0;
+for (let i = 0; i < palavraMisteriosa.length; i++) {
+  if (
+    palavraMisteriosa[i] == "a" ||
+    palavraMisteriosa[i] == "e" ||
+    palavraMisteriosa[i] == "i" ||
+    palavraMisteriosa[i] == "o" ||
+    palavraMisteriosa[i] == "u"
+  ) {
+    qtdVogal++;
+  }
+}
+console.log(qtdVogal);
