cb5aacec60d4b0f4f16fdb26774bc3a092041533
nelynely <nelly.nf54@gmail.com>
2025-04-21 11:39:02 -0300

    Create array-numeros.ts

---
+++ src/array-numeros.ts
@@ -0,0 +1,14 @@
+let array: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let pares: number[] = [];
+let impares: number[] = [];
+for (let i: number = 0; i < array.length; i++) {
+  if (array[i] % 2 == 0) {
+    pares.push(array[i]);
+  } else {
+    impares.push(array[i]);
+  }
+}
+
+console.log(`Números pares: [ ${pares} ]`);
+console.log(`Números ímpares: [ ${impares}]`);
