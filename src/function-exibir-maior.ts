0dc7899b32556d5bb1a3987a251f337afcaab7dc
nelynely <nelly.nf54@gmail.com>
2025-04-21 11:42:57 -0300

    Create function-exibir-maior.ts

---
+++ src/function-exibir-maior.ts
@@ -0,0 +1,13 @@
+let numerosArray2: number[] = [20, 10, 130, 50, 40];
+
+function exibirMaior(arr: number[]) {
+  let maior: number = arr[0];
+  for (let i: number = 0; i < arr.length; i++) {
+    if (arr[i] > maior) {
+      maior = arr[i];
+    }
+  }
+  console.log(maior);
+}
+
+exibirMaior(numerosArray2);
