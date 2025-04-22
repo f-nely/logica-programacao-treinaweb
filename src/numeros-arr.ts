d8d7aa25b33cdc0818d6e45a4135899c2676f6f1
nelynely <nelly.nf54@gmail.com>
2025-04-22 18:40:21 -0300

    Create new function

--- src/numeros-arr.ts
+++ src/numeros-arr.ts
@@ -1,16 +1,18 @@
-let numerosArray: number[] = [5, 80, 4];
-//let numerosArray: number[] = [5, 80, -4];
+//let numerosArray: number[] = [5, 80, 4];
+let numerosArray: number[] = [5, 80, -4];
 let temNegativo: boolean = false;
 
-for (let i: number = 0; i < numerosArray.length; i++) {
-  if (numerosArray[i] < 0) {
-    temNegativo = true;
-  } else {
-    temNegativo = false;
+function findNegativeNumbers(arr: number[]): boolean {
+  for (let i: number = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      temNegativo = true;
+      break;
+    }
   }
+  return temNegativo;
 }
 
-if (temNegativo) {
+if (findNegativeNumbers(numerosArray)) {
   console.log("Esse array possui números negativos");
 } else {
   console.log(numerosArray);
