d5fde3a99251cdc7d769d8eefbc2c00944fffe24
nelynely <nelly.nf54@gmail.com>
2025-04-24 15:41:44 -0300

    Create function thereAreRepeatedNumbers

--- src/arr1.ts
+++ src/arr1.ts
@@ -4,17 +4,20 @@ let primeiroArray: number[] = [2, 10, 5];
 let segundoArray: number[] = [3, 15, 5];
 
 let temNumerosRepetidos: boolean = false;
-for (let i: number = 0; i < primeiroArray.length; i++) {
-  for (let j: number = 0; j < segundoArray.length; j++) {
-    if (primeiroArray[i] == segundoArray[j]) {
-      temNumerosRepetidos = true;
-    } else {
-      temNumerosRepetidos = false;
+//there are repeated numbers in the array
+function thereAreRepeatedNumbers(arr1: number[], arr2: number[]): boolean {
+  for (let i: number = 0; i < arr1.length; i++) {
+    for (let j: number = 0; j < arr2.length; j++) {
+      if (arr1[i] == arr2[j]) {
+        temNumerosRepetidos = true;
+        break;
+      }
     }
   }
+  return temNumerosRepetidos;
 }
 
-if (temNumerosRepetidos) {
+if (thereAreRepeatedNumbers(primeiroArray, segundoArray)) {
   console.log("Existem números repetidos.");
 } else {
   console.log(primeiroArray);
