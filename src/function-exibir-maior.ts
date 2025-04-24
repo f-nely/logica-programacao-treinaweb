9b46d533ebddaa2b736460377c552df11e50e8be
nelynely <nelly.nf54@gmail.com>
2025-04-24 15:45:34 -0300

    Add return and type to function exibeMaior

--- src/function-exibir-maior.ts
+++ src/function-exibir-maior.ts
@@ -1,13 +1,13 @@
 let numerosArray2: number[] = [20, 10, 130, 50, 40];
 
-function exibirMaior(arr: number[]) {
+function exibirMaior(arr: number[]): number {
   let maior: number = arr[0];
   for (let i: number = 0; i < arr.length; i++) {
     if (arr[i] > maior) {
       maior = arr[i];
     }
   }
-  console.log(maior);
+  return maior;
 }
 
-exibirMaior(numerosArray2);
+console.log(exibirMaior(numerosArray2));
